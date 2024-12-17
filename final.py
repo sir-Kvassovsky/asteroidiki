ceb95afa0993126cff980b7d017015564d88df3f
sir-Kvassovsky <159709815+sir-Kvassovsky@users.noreply.github.com>
2024-12-17 12:39:05 +0300

    movement done (for now)

--- final.py
+++ final.py
@@ -11,13 +11,12 @@ height = 600
 canvas = tk.Canvas(root, width=width, height=height, bg="white")
 
 ship = canvas.create_line(400, 280, 400, 320, arrow='first', arrowshape=(40, 50, 15))
-a = 3.14 * 3/2
+a = math.pi * 3/2
 speed = 1 
 
-def start_move(direction):
+def start_move(event):
     global moving
     moving = True
-    move(direction)
 
 def endless_window(x1, y1, x2, y2, obj):
     if x2 < 0:
@@ -29,47 +28,50 @@ def endless_window(x1, y1, x2, y2, obj):
     elif y1 > 600:
         canvas.move(obj, 0, -(y2 - y1 + 600))
 
-def stop_move(direction):
-    global moving, speed
+def stop_move(event):
+    global moving
     moving = False
-    speed = 1
 
 def speed_change():
     global speed
-    speed *= 1.003
+    if moving:
+        speed = min(speed + 0.05, 10)
+    else:
+        speed = max(speed - 0.1, 0) 
 
-def rotation(x1, y1, x2, y2, direction):
-    global ship, a
-    canvas.delete(ship)
+
+def rotation(direction):
+    global a
     if direction == "Left":
         i = -1
     elif direction == "Right":
         i = 1
-    a+= 0.01 * i
+    x1, y1, x2, y2 = canvas.coords(ship)
+    a+= 0.1 * i
     x1 = cos(a) * 40 + x2
     y1 = sin(a) * 40 + y2
-    ship = canvas.create_line(x1, y1, x2, y2, arrow='first', arrowshape=(40, 50, 15))
+    canvas.coords(ship, x1, y1, x2, y2)
 
-def move(direction):
-    global ship, a, speed
+def rotation_left(event):
+    rotation("Left")
+
+def rotation_right(event):
+    rotation("Right")
+
+def move():
+    global ship, a, moving
     x1, y1, x2, y2 = canvas.coords(ship)
     speed_change()
-    if direction != "Up":
-        rotation(x1, y1, x2, y2, direction)
-    elif direction == "Up":
-        canvas.move(ship, speed*speed*cos(a), speed*speed*sin(a))
+    canvas.move(ship, speed*cos(a), speed*sin(a))
     endless_window(x1, y1, x2, y2, ship)
-    root.after(20, lambda: move(direction))  
+    root.after(10, move)
 
-
-moving = False 
-root.bind("<KeyPress-Up>", lambda e: start_move("Up"))
-root.bind("<KeyRelease-Up>", lambda e: stop_move("Up"))
-root.bind("<KeyPress-Left>", lambda e: start_move("Left"))
-root.bind("<KeyRelease-Left>", lambda e: stop_move("notUp"))
-root.bind("<KeyPress-Right>", lambda e: start_move("Right"))
-root.bind("<KeyRelease-Right>", lambda e: stop_move("notUp"))
-
+moving = False
+root.bind("<KeyPress-Up>", start_move) 
+root.bind("<KeyRelease-Up>", stop_move) 
+root.bind("<Left>", rotation_left)
+root.bind("<Right>", rotation_right)
+move()
 
 canvas.pack()
 root.mainloop()
